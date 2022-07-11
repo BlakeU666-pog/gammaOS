8c01d730b66b8296b8667224d3b5f57d68287a7d
BlakeU666 <96388879+BlakeU666-pog@users.noreply.github.com>
2022-07-11 15:52:08 +0200

    Added minimal keyboard support
    Now you can type in anything you want. I also made Enter work so you can go to a newline now. I still have to add Backspace support though.

--- src/kernel.asm
+++ src/kernel.asm
@@ -1,86 +1,103 @@
-;; =======================================================================================================
-;;
-;;
-;;       /-----------\       /-\        /------\        /------\ /------\        /------\       /-\
-;;       |-----------|      /-_-\       |-------\      /-------| |-------\      /-------|      /-_-\
-;;       |--|     |__|     /-| |-\      |----\---\    /---/----| |----\---\    /---/----|     /-| |-\
-;;       |--|   _____     /--|_|--\     |----|\---\  /---/|----| |----|\---\  /---/|----|    /--|_|--\
-;;       |--|  |___--\   /---------\    |----| \---\/---/ |----| |----| \---\/---/ |----|   /---------\
-;;       |--|______|-|  /---/   \---\   |----|  \______/  |----| |----|  \______/  |----|  /---/   \---\
-;;       \___________/ /___/     \___\  |____|            |____| |____|            |____| /___/     \___\
-;;
-;;
-;; =======================================================================================================
+;; gammaOS is a project developed by Dominik Mazurek
 
-	[org 0x7c00]
+[org 0x7c00]
 
-	jmp k_init
+jmp k_init
 
 ;; Function for clearing screen. It basically just scrolls up the screen to get rid of all text.
 
 clear_screen:
-	mov ah, 07h
-	mov al, 0
-	int 10h
-	ret
+        mov ah, 07h
+        mov al, 0
+        int 10h
+        ret
 
 ;; Function for settings video mode. You have to provide what video mode you want before calling this function.
 
 set_videomode:
-	mov ah, 00h
-	int 10h
-	ret
+        mov ah, 00h
+        int 10h
+        ret
 
 ;; Function for printing a character
 
 print_char:
-	mov ah, 0Eh
-	int 10h
-	ret
+        mov ah, 0Eh
+        int 10h
+        ret
 
 ;; Function for printing a string
 
 print_string:
-	mov ah, 0Eh
+        mov ah, 0Eh
 .next_char:
-	lodsb
-	cmp al, 0
-	je .string_done
-	int 10h
-	jmp .next_char
+        lodsb
+        cmp al, 0
+        je .string_done
+        int 10h
+        jmp .next_char
 .string_done:
-	ret
+        call newline
+        ret
+
+;; Cursor
+
+get_c_position:
+        mov ah, 03h
+        mov bh, 0
+        int 10h
+        ret
+
+set_c_position:
+        mov ah, 02h
+        mov bh, 0
+        ;; You have to manually set DH (row) and DL (column) for this to work.
+        int 10h
+        ret
+
+;; Keyboard support
+
+newline:
+        call get_c_position
+        mov dl, 0x0
+        add dh, 1
+        call set_c_position
+        ret
+
+get_key:
+        mov ah, 00h
+        int 16h
+
+        cmp ah, 0x1C ;; Check if enter was pressed
+        je newline
+
+        call print_char
+        ret
 
 ;; Kernel main functions
 
 k_init:
-	call clear_screen
-	mov al, 03h
-	call set_videomode
+        call clear_screen
+        mov al, 03h
+        call set_videomode
 
-	mov si, WELCOME
-	call print_string
+        mov si, STARTING
+        call print_string
 
-	jmp k_mainloop
+        jmp k_mainloop
 
 k_exit:
-	call clear_screen
-	cli
-	hlt
-	jmp $
+        call clear_screen
+        cli
+        hlt
+        jmp $
 
-k_mainloop: ;; It just waits for a key, that's all. Lmao
-	mov ah, 00h
-	int 16h
-	cmp ah, 0x48
-	je k_exit
-	call print_char
+k_mainloop: ;; It just waits for a key for now.
+        call get_key
 
-	jmp k_mainloop
+        jmp k_mainloop
 
-;; Text message variables
-WELCOME db 'WELCOME TO GAMMA!', 0
+STARTING db 'Starting gammaOS', 0
 
-;; Make OS bootable
 times 510-($-$$) db 0
 dw 0xAA55
